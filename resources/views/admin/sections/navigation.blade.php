441543513b15fd725fa548b5fc7cb13c36a3deab
Diego Meretta <santicluke@gmail.com>
2021-06-17 10:16:21 -0300

    *start deleted

--- resources/views/admin/sections/navigation.blade.php
+++ resources/views/admin/sections/navigation.blade.php
@@ -24,17 +24,6 @@
         <!-- sidebar menu -->
         <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-                <h3>{{ __('views.backend.section.navigation.sub_header_0') }}</h3>
-                <ul class="nav side-menu">
-                    <li>
-                        <a href="{{ route('admin.dashboard') }}">
-                            <i class="fa fa-home" aria-hidden="true"></i>
-                            Start
-                        </a>
-                    </li>
-                </ul>
-            </div>
-            <div class="menu_section">
                 <h3>{{ __('views.backend.section.navigation.sub_header_1') }}</h3>
                 <ul class="nav side-menu">
                     <li>
